59ca6b89fb775f3ec1df769e1da7f66fb87d2781
Zainab Iftikhar <zainabiftikhar1204@gmail.com>
2021-11-01 21:07:37 +0500

    Created Model for Custom User Fields

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='email',
+                              max_length=150, unique=True)
+    phone = models.CharField(null=True, max_length=20)
+    REQUIRED_FIELDS = ['username', 'phone', 'first_name', 'last name']
+    USERNAME_FIELD = 'email'
+
+    def get_username(self):
+        return self.email
