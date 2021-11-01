900bbb1ed5d195abe932d036ca5659e2a56ca695
Zainab Iftikhar <zainabiftikhar1204@gmail.com>
2021-11-01 15:33:34 +0500

    Testing Commit

--- authapi/views.py
+++ authapi/views.py
@@ -6,6 +6,6 @@ from datetime import date, datetime
 @api_view(['GET'])
 def index(request):
     date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    msg = "Server is Live. Current Time is: "
+    msg = "Server is Live! Current Time is: "
     return Response(data=msg + date, status=status.HTTP_200_OK)
 
